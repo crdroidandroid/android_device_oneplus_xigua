1b073bde3aab7b806d92ec97fa9267c4e55b34af
pjgowtham <pjgowtham@gmail.com>
2025-07-30 17:03:04 +0530

    xigua: Build composer with support for userspace dim layer
    Change-Id: Ib3515e9a93c62c31082243cd09a1782503761791
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -38,6 +38,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
+$(call soong_config_set_bool,qtidisplay,oplus_udfps,true)
 
 # LiveDisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,true)
